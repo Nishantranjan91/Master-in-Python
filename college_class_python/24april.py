5484d1b688cec8b274de3dd3f4a0bb5509de891e
Nishant <nishantranjj91555@gmail.com>
2025-04-24 16:20:23 +0530

    use of write close read etc

--- college_class_python/24april.py
+++ college_class_python/24april.py
@@ -5,22 +5,22 @@ file=open("abc.txt","r")
 content=file.read()
 print(content)
 file.close()
-# fil
-# file = open("xyz.txt" , 'r');
-# file.write("hello again")
-# content=file.read()
-# print(content)
-# file.close()
-# file=open("abc.txt",'a')
-# file.write("my name is nishant")
-# file.close()
-# file=open("abc.txt","r")
-# content=file.read()
-# print(content)
-# print("my name is %s and weight is %d kg"%("ds",70))
+ fil
+ file = open("xyz.txt" , 'r');
+ file.write("hello again")
+ content=file.read()
+ print(content)
+ file.close()
+ file=open("abc.txt",'a')
+ file.write("my name is nishant")
+ file.close()
+ file=open("abc.txt","r")
+ content=file.read()
+ print(content)
+ print("my name is %s and weight is %d kg"%("ds",70))
 name="ds"
 wt=70
-# print("my name is { ds} and my wt is{70}kg".format(name,70))
-# def fun(name):
-#     print("my name is"+name)
-#     print(f'my name is {name}')
+ print("my name is { ds} and my wt is{70}kg".format(name,70))
+ def fun(name):
+     print("my name is"+name)
+     print(f'my name is {name}')
