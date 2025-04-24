de453982d07d5b179a7abfbf6f05a3f5c9772d5f
Nishant <nishantranjj91555@gmail.com>
2025-04-24 18:32:21 +0530

    conditional operator

--- day0python/07_operators/01_operators.py
+++ day0python/07_operators/01_operators.py
@@ -1,9 +1,14 @@
 a=55
 b=11
 #arithmetic operators
-print("a+b=",a+b)
-print("a-b=",a-b)
-print("a*b=",a*b)
-print("a/b=",a/b)
-print("a%b=",a%b)
-print("a//b=",a//b)
+# print("a+b=",a+b)
+# print("a-b=",a-b)
+# print("a*b=",a*b)
+# print("a/b=",a/b)
+# print("a%b=",a%b)
+# print("a//b=",a//b)
+#conditional operators
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
