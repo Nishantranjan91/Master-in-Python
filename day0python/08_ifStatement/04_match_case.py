f82cbbb4d67c320ec1f5d6d0a3391eb0414605ee
Nishant <nishantranjj91555@gmail.com>
2025-04-25 13:09:22 +0530

    match case statement

---
+++ day0python/08_ifStatement/04_match_case.py
@@ -0,0 +1,11 @@
+a=int(input("enter your lucky number:"))
+match a:
+    case 1:
+        print("you won a charger")
+    case 3:
+        print("you won 3$")
+    case 6:
+        print("you won a camera")
+    case _:
+
+        print("better luck for next time")    
