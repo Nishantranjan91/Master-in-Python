7304824a1f57acda2a756963898bc42e96995467
Nishant <nishantranjj91555@gmail.com>
2025-04-26 18:47:54 +0530

    pass

--- day0python/09_loops/04_continue.py
+++ day0python/09_loops/04_continue.py
@@ -1,4 +1,8 @@
-for i in range(0,20):
-    if i==10:
-        continue
-    print(i)
+# for i in range(0,20):
+#     if i==10:
+#         continue
+#     print(i)
+i=3
+if i==3:
+  pass
+print("end of program")
