61bbc72d296c481279a8de1ccb7625f185167b4b
Nishant <nishantranjj91555@gmail.com>
2025-04-30 19:04:07 +0530

    split function

--- day0python/10_string/04_string.py
+++ day0python/10_string/04_string.py
@@ -1,4 +1,9 @@
-s="hello world"
-a=len(s)
-print(a)
-print(s.upper(), s)
+# s="hello world"
+# a=len(s)
+# print(a)
+# print(s.upper(), s)
+# print(s.lower())
+# print(s.capitalize)
+# print(s.title)
+text="apple, banana,pineapple"
+print(text.split(","))
