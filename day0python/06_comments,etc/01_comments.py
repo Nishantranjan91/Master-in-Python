88e78ff3178d0f389a13fbc2ce169c34366f4995
Nishant <nishantranjj91555@gmail.com>
2025-05-01 23:04:45 +0530

    averavg of 3 numbers

--- day0python/06_comments,etc/01_comments.py
+++
@@ -1,5 +0,0 @@
-print("hey how are you ?")
-   # Arjun please finish this code
-  ''' i am nishant ranjan,
-   # Rohan, please review what arjun was done
-   '''
---
+++ day0python/12_functionsNmodules/01_average.py
@@ -0,0 +1,5 @@
+a=4
+b=2
+c=1
+average=(a+b+c)/3
+print(average)
