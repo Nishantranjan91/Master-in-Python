11847b95cf9c6fa0a05087e53e74dbbcae404dcd
Nishant <nishantranjj91555@gmail.com>
2025-05-02 10:06:43 +0530

    functions in python

--- day0python/12_functionsNmodules/01_average.py
+++ day0python/12_functionsNmodules/01_average.py
@@ -3,7 +3,13 @@
 # c=1
 # average=(a+b+c)/3
 # print(average)
-def average(a,b,c):
+# de
+def average (a,b,c):
     d=(a+b+c)/3.0
-    print(d)
-average(3,5,1)    
+    # print(d)
+    return d
+o1=average(3,5,1)
+o2=average(4,2,1)
+print(o1)    
+print(o2)
+
