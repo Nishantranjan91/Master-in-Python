4973729f980fdbecd8fb1c63de65b80e7bbc2722
Nishant <nishantranjj91555@gmail.com>
2025-05-02 08:43:06 +0530

    a simple code of function

--- day0python/12_functionsNmodules/01_average.py
+++ day0python/12_functionsNmodules/01_average.py
@@ -1,5 +1,9 @@
-a=4
-b=2
-c=1
-average=(a+b+c)/3
-print(average)
+# a=4
+# b=2
+# c=1
+# average=(a+b+c)/3
+# print(average)
+def average(a,b,c):
+    d=(a+b+c)/3.0
+    print(d)
+average(3,5,1)    
