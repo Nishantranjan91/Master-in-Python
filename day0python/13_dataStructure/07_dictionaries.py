d96a82852281cc271d681b90eb4624d43dfb7357
Nishant <nishantranjj91555@gmail.com>
2025-05-03 18:49:26 +0530

    dictionary method of writting a table

--- day0python/13_dataStructure/07_dictionaries.py
+++ day0python/13_dataStructure/07_dictionaries.py
@@ -1,5 +1,7 @@
-marks={"nishant":90, "santosh":95, "adnan":94, "fardin":92}
-print(marks, type(marks))
-# print(marks["santosh"])
-marks["nishant"]=55
-print(marks)
+# marks={"nishant":90, "santosh":95, "adnan":94, "fardin":92}
+# print(marks, type(marks))
+# # print(marks["santosh"])
+# marks["nishant"]=55
+# print(marks)
+table_of_5={i:5*i for i in range(1,11) }
+print(table_of_5)
