d3a312e0d36fdb5903f2c6520c77c54187df0ea0
Nishant <nishantranjj91555@gmail.com>
2025-05-03 10:52:43 +0530

    short cut method of writting the  table

--- day0python/13_dataStructure/03_listMethod.py
+++ day0python/13_dataStructure/03_listMethod.py
@@ -1,5 +1,7 @@
-marks=[22,40,38,33,25]
-extra_marks=[22,33,44,55,99]
-print(marks)
-marks.extend(extra_marks)
-print(marks)
+# marks=[22,40,38,33,25]
+# extra_marks=[22,33,44,55,99]
+# print(marks)
+# marks.extend(extra_marks)
+# print(marks)
+table=[5*i for i in range(1,11)]
+print(table)
