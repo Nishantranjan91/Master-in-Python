f7f0b868b2c21400d263b95cc907386140b15d32
Nishant <nishantranjj91555@gmail.com>
2025-05-06 07:54:39 +0530

    basics of getters

--- day0python/15_advancedPythonConcept/03_gettersNsetters.py
+++ day0python/15_advancedPythonConcept/03_gettersNsetters.py
@@ -2,7 +2,11 @@ class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
+    def first_name(self):
+        l = self.name.split(" ")
+        print(l)
+        return l[0]    
 
-e = Employee("jack", 34555) 
-e.projects = 6
-print(e.projects)
+e = Employee("jack Doe", 34555) 
+
+print(e.first_name())
