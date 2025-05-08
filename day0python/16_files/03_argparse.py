132d6375e00d81409991832f6073234f734ce4bd
Nishant <nishantranjj91555@gmail.com>
2025-05-08 11:11:40 +0530

    all about argparse

--- day0python/16_files/03_argparse.py
+++ day0python/16_files/03_argparse.py
@@ -7,3 +7,18 @@ parser.add_argument("num2",type=float,help="second number")
 parser.add_argument("operation", choices=["add", "sub", "div", "mul"], help="peration to perform")
 args = parser.parse_args()
 print(args)
+
+if(args.operation == "add"):
+    print(f"The result is {args.num1+args.num2}")
+
+elif(args.operation == "sub"):
+    print(f"The result is {args.num1-args.num2}")
+
+elif(args.operation == "mul"):
+    print(f"The result is {args.num1*args.num2}")
+
+elif(args.operation == "div"):
+    print(f"The result is {args.num1/args.num2}")
+
+else:
+    print("some error occured")   
