e1017cda15695d1e8bf4af9a684f61280bbf992f
Nishant <nishantranjj91555@gmail.com>
2025-05-09 19:25:23 +0530

    regular expression

---
+++ day0python/17_external/03_regulaExpression.py
@@ -0,0 +1,8 @@
+import re
+text = "The quick brown fox jumps over the lazy dog."
+match = re.search("brown",text)
+if match:
+    print(match)
+    print("match found !")
+    print("start index:",match.start())
+    print("end endex:",match.end())
