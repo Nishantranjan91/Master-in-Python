8b4f6f1c3fa4d396d1fe52245f37e84610e763d2
Nishant <nishantranjj91555@gmail.com>
2025-05-15 08:27:42 +0530

    news.api

---
+++ day0python/18_hands_on_python_project/02_newsApi.py
@@ -0,0 +1,6 @@
+query = "artificial intelligence"
+api = "f73ab46547794db986c24c2a1f06c1c7"
+
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-04-10&sortBy=publishedAt&apiKey=API_KEY"
+
+print(url)
